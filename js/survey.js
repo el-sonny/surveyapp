a2c43de9f23d8d14e45ff3d78cdaace5a941c5ad
urielaero <aero.uriel@gmail.com>
2013-12-20 17:51:16 +0800

    pantalla inicial y json

---
+++ js/survey.js
@@ -0,0 +1,58 @@
+var survey = {
+	1:{
+		es:'La Atención y Servicio de nuestro Ejecutivo de Ventas en el Aeropuerto fue:', 
+		en:'The Service provided by our Sales Executive at the Airport was:'
+	},
+
+	2:{
+		es:'La atención del representante en el aeropuerto fue:', 
+		en:'The meet and greet service at the Airport was:'
+	},
+
+	3:{
+		es:'Por favor califique el servicio de nuestro conductor', 
+		en:'How do you rate the Service from our driver?'
+	},
+
+	4:{
+		es:'La apariencia personal de nuestro staff fue:', 
+		en:'Our staff appearance was:'
+	},
+
+	5:{
+		es:'¿Cómo califica la calidad del servicio de nuestro Representante en el Hotel?', 
+		en:'Please rate the Service quality of our representative at your hotel:'
+	},
+
+	6:{
+		es:'La variedad de excursiones que le ofreció el representante en su hotel fue:', 
+		en:'The sightseeing choice offered by our representantive at your hotel was:'
+	},
+
+	7:{
+		es:'¿Estuvimos a tiempo para su traslado de salida?', 
+		en:'Were we on time for your return transfer?'
+	},
+
+	8:{
+		
+		es:'¿Usaría de nuevo Yellow Transfers?',
+		en:'Would you use again Yellow Transfers'
+	},
+
+	9:{
+		es:'Adquirió Tours', 
+		en:'Did you take tours'
+	},
+
+	10:{
+		es:'', 
+		en:''
+	},
+
+	11:{
+		es:'', 
+		en:''
+	}
+
+};
