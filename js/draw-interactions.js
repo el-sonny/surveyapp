6307ed11239019dc2e5c7bc2eda1bca56a21f92e
urielaero <aero.uriel@gmail.com>
2013-12-31 21:08:04 +0800

    show ,,,

--- js/draw-interactions.js
+++ js/draw-interactions.js
@@ -2,31 +2,59 @@ google.load("visualization", "1", {packages:["corechart"]});
 //google.setOnLoadCallback(drawChart);
 
 $(function(){
-	drawQuestion('una pregunta');
+	//draw all questions
+	/*
+	$('.box').on('click',function(e){
+		e.preventDefault();
+		var box = $(this);
+		changeQuestion(box.html());
+		$('.box.on').removeClass('on').addClass('completed');
+		box.addClass('on');
+	});
+	*/
+	$('.box').on('click',function(e){
+		e.preventDefault();
+		$('.box.completed').removeClass('completed');
+		$(this).addClass('completed');
+		var index = $(this).html();
+		url = "http://yellowadmin.projects.spaceshiplabs.com/api/getDataByQuestion/"
+		$.ajax({
+			url: url,
+			crossDomain : true,
+			type:'post',
+			dataType : 'jsonp',
+			data : {
+				question:index
+			}, 
+			success:drawQuestion
+		})
+		//change question
+		$('.survey p').html(survey[index].en);
+	});
+
+	$('.box.completed').trigger('click');
 });
 
 function drawQuestion(question){
-	var callback = drawWithData(question);
-	google.load("visualization", "1",{callback:callback});
+	var data = getQuestionTable(question),
+	options = {
+		pieHole: 0.2,
+		backgroundColor: "transparent"
+	};
+	var chart = new google.visualization.PieChart(document.getElementById('chart'));
+	//color
+
+
+	chart.draw(data,options);	
 }
 
-function getQuestionData(question){
-	return google.visualization.arrayToDataTable([
-		['Task', 'number of data'],
-		['good', 11],
-		['medium', 2],
-		[question,  2]
-	]);
-}
-
-function drawWithData(question){
-	var data = getQuestionData(question);
-	return function(){
-		var options = {
-			title: "My Daily activities",
-			is3D: true
-		};
-		var chart = new google.visualization.PieChart(document.getElementById('chart'));
-		chart.draw(data,options);
+function getQuestionTable(question){
+	var table = [
+		['Task', 'number of data']
+	];
+	for(var i in question){
+		table.push([i,question[i]]);
 	}
+
+	return google.visualization.arrayToDataTable(table);
 }
