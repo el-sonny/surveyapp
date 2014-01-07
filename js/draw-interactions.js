b08f8e339d5226b7a706f03f4618202a9be1d229
urielaero <aero.uriel@gmail.com>
2014-01-07 19:19:25 +0800

    tabla

--- js/draw-interactions.js
+++ js/draw-interactions.js
@@ -1,4 +1,5 @@
 google.load("visualization", "1", {packages:["corechart"]});
+google.load('visualization', '1', {packages:['table']});
 //google.setOnLoadCallback(drawChart);
 
 $(function(){
@@ -17,7 +18,6 @@ $(function(){
 			timeTo += separate + (timeT.find("input")[i].value).toString();
 			separate = "-";
 		}
-		console.log(timeFrom,timeTo);
 		url = "http://yellowadmin.projects.spaceshiplabs.com/api/getDataByQuestion/"
 		//url = "http://yellowadmin/api/getDataByQuestion/"
 		$.ajax({
@@ -39,41 +39,74 @@ $(function(){
 	$('.box.completed').trigger('click');
 
 	$('.up').on('click',function(){
-		$(this).parent().prev()[0].stepUp();
+		//$(this).parent().prev()[0].stepUp();
+		var input = $(this).parent().prev()[0];
+		if(input.value == input.max)
+			input.value = input.min-1;
+		input.stepUp();
 	});
 
 	$('.down').on('click',function(){
-		$(this).parent().prev()[0].stepDown();
+		var input = $(this).parent().prev()[0];
+		if(input.value == input.min)
+			input.value = parseInt(input.max)+1;
+		input.stepDown();
+	});
+
+	$('.month + .arrows,.year + .arrows').on('click',function(){
+		select= $(this).parent().parent().find('input'),
+		month = select[1].value - 1,
+		day = 27,
+		year = select[2].value,
+		date = new Date(year,month,day);
+		lastDayFind = false;
+		while(!lastDayFind){
+			date.setDate(++day);
+			if(date.getMonth()!=month){
+				lastDayFind = true;
+				day--;
+			}
+		}
+		console.log("last: "+day);
+		console.log(day,year,month,date);
+		select[0].max = day;
+		if(select[0].value > day)
+			select[0].value = day;
 	});
 });
 
 function drawQuestion(question){
 	var data = getQuestionTable(question),
-	options = {
-		pieHole: 0.5,
-		backgroundColor: 'transparent',
-		colors:['#FFDD02','#DBBE0B','#A38F0A'],
-		pieSliceBorderColor:'none',
-		pieSliceTextStyle:{color:'#888984',fontSize:18,fontWeight:'bold'},
-		chartArea:{width:350,height:350,left:90},
-		legend:{
-			textStyle:{
-				fontSize:20,
-				color:'#888984'
+	options, chart;
+	if($('.completed').html()==10){
+		chart = new google.visualization.Table(document.getElementById('chart'));
+		options = {showRowNumber: true}
+	}else{
+		options = {
+			pieHole: 0.5,
+			backgroundColor: 'transparent',
+			colors:['#FFDD02','#DBBE0B','#A38F0A'],
+			pieSliceBorderColor:'none',
+			pieSliceTextStyle:{color:'#888984',fontSize:18,fontWeight:'bold'},
+			chartArea:{width:350,height:350,left:90},
+			legend:{
+					textStyle:{
+					fontSize:20,
+					color:'#888984'
+				},
+				position:'right',
+				alignment:'center'
 			},
-			position:'right',
-			alignment:'center'
-		},
-		height:300
-	};
-	var chart = new google.visualization.PieChart(document.getElementById('chart'));
+			height:300
+		};
+		chart = new google.visualization.PieChart(document.getElementById('chart'));
+	}
 	chart.draw(data,options);
-	$('path + text').each(moveText);
 }
 
 function getQuestionTable(question){
 	var table = [
-		['Task', 'number of data']
+		['Name', 'percentage']
 	];
 	for(var i in question){
 		table.push([i,question[i]]);
@@ -106,6 +139,8 @@ function moveText(i,select){
 	bounding = path[0].getBoundingClientRect();
 	select.attr("x",parseFloat(w)+(section[s][0]*(55)));
 	select.attr("y",parseFloat(h)+(section[s][1]*(55)));
+}
 
+function resetTime(select){
 
 }
